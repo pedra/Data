2922830dfdb7b1b32fedf0d56f42d98884a07752
Bill Rocha <ahcordesign@gmail.com>
2014-02-17 10:18:15 -0300

    toIniFile
    add function toIniFile()

--- src/Neos/Data/Config.php
+++ src/Neos/Data/Config.php
@@ -58,5 +58,38 @@ class Config {
         }
         return $st;
     }
+    
+     /**
+     * Cria um arquivo ".ini"
+     *
+     * @param Array $ini        Array contendo os dados a serem convertidos
+     * @param String $file  Caminho e nome do arquivo ".ini"
+     *
+     * @return Bool|String  Se $file for indicado retorna o status da criação/grvação do arquivo
+     *    Se $file não for indicado retorna uma string com os dados convertidos
+     */
+    
+    static function toIniFile($file = null, $type = 'user'){
+        if(!is_array(static::$$type)) return false;
+
+        $o = '';
+        foreach(static::$$type as $k => $v){
+            $o .= '[' . $k . "]\r\n";
+            //segundo nó
+            if(is_array($v)){
+                foreach($v as $_k => $_v){
+                    //terceiro nó
+                    if(is_array($_v)){
+                        foreach($_v as $__k => $__v){
+                            if(is_array($__v)) $__v = print_r($__v, true);
+                            $o .= "\t" . $_k . '[' . $__k . '] = ' . (is_numeric($__v) ? $__v : '"' . $__v . '"') . "\r\n";
+                        }
+                    } else $o .= "\t" . $_k . ' = ' . (is_numeric($_v) ? $_v : '"' . $_v . '"') . "\r\n";
+                }
+            }
+        }
+        if($file != null) return file_put_contents($file, $o);
+        else return $o;
+    }
 
 }
